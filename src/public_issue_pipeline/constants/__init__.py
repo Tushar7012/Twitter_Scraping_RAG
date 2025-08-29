5608296f4f7ed11b97b32acbe477ad38e9142b31
Tushar <td220627@gmail.com>
2025-08-29 14:55:18 +0530

    Constants Added --

---
+++ src/public_issue_pipeline/constants/__init__.py
@@ -0,0 +1,10 @@
+QUERY = "potholes OR #powercut OR pollution OR governance -is:retweet lang:en"
+MAX_RESULTS = 100
+TWEET_FIELDS = ["created_at", "public_metrics", "conversation_id"]
+USER_FIELDS = ["username"]
+
+DB_NAME = "your_db_name"
+DB_USER = "your_db_user"
+DB_PASSWORD = "your_db_password"
+DB_HOST = "localhost"
+DB_PORT = "5432"
